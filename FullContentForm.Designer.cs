038879089eb3cd27263c9ef340023e28e8409d77
PUGAZHENTHI THANIARASU I R <pugazhenthiir2002@gmail.com>
2024-03-27 18:04:48 +0530

    Add files via upload

---
+++ FullContentForm.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace DataGridViewPractice
+{
+    partial class FullContentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.closeButton1 = new DataGridViewPractice.CloseButton();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.closeButton1);
+            this.panel1.Location = new System.Drawing.Point(142, 77);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(112, 223);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 100);
+            this.panel2.TabIndex = 1;
+            // 
+            // closeButton1
+            // 
+            this.closeButton1.BackColor = System.Drawing.Color.Transparent;
+            this.closeButton1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.closeButton1.Location = new System.Drawing.Point(50, 0);
+            this.closeButton1.Name = "closeButton1";
+            this.closeButton1.Size = new System.Drawing.Size(100, 100);
+            this.closeButton1.TabIndex = 0;
+            // 
+            // FullContentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 365);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FullContentForm";
+            this.Text = "FullContentForm";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private CloseButton closeButton1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
---
+++ FullContentViewForm.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataGridViewPractice
+{
+    public partial class FullContentViewForm : Form
+    {
+        public string TitleName { get; set; }
+        public string Content
+        {
+            get
+            { return content; }
+            set
+            { content = value; label1.Text = value; }
+        }
+       
+        public FullContentViewForm()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+            titleFont = new Font(new FontFamily("Microsoft PhagsPa"), 20, FontStyle.Bold);
+            typeof(Label).InvokeMember("DoubleBuffered", BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.NonPublic, null, label1, new object[] { true });
+        }
+
+        private void fullContentTitleBarPanel_Paint(object sender, PaintEventArgs e)
+        {
+            headCnt++;
+            SFormat = new StringFormat();
+            SFormat.Alignment = StringAlignment.Near;
+            SFormat.LineAlignment = StringAlignment.Center;
+
+            titleBrush = new SolidBrush(Color.FromArgb(52, 104, 192));
+            rec = new Rectangle(0, 0, fullContentTitleBarPanel.Width, fullContentTitleBarPanel.Height);
+
+            e.Graphics.DrawString(TitleName, titleFont, titleBrush, rec, SFormat);
+        }
+
+
+        private void closeButton1_Click(object sender, EventArgs e)
+        {
+            NotificationTemplate.isFullContentDisplayed = false;
+            this.Close();
+        }
+
+        private void FullContentFormPaint(object sender, PaintEventArgs e)
+        {
+            cnt++;
+            e.Graphics.SmoothingMode = SmoothingMode.HighQuality;
+            Brush b = new SolidBrush(Color.FromArgb(253, 240, 209));
+            Rectangle rectangle = new Rectangle(1, 1, this.Width - 2, this.Height - 2);
+            FillRoundRectangle(e.Graphics, rectangle, b, 10);
+            b.Dispose();
+        }
+
+        private static GraphicsPath GetRoundRectangle(Rectangle rectangle, int r)
+        {
+            int l = 2 * r;
+            GraphicsPath gp = new GraphicsPath();
+            gp.StartFigure();
+            gp.AddArc(new Rectangle(rectangle.Right - l, rectangle.Y, l, l), 270F, 90F);
+            gp.AddArc(new Rectangle(rectangle.Right - l, rectangle.Bottom - l, l, l), 0F, 90F);
+            gp.AddArc(new Rectangle(rectangle.X, rectangle.Bottom - l, l, l), 90F, 90F);
+            gp.AddArc(new Rectangle(rectangle.X, rectangle.Y, l, l), 180F, 90F);
+            gp.CloseAllFigures();
+            return gp;
+        }
+
+        private void FillRoundRectangle(Graphics g, Rectangle rectangle, Brush b, int r)
+        {
+            Pen p = new Pen(Color.Black, 3);
+            p.DashStyle = DashStyle.Solid;
+            GraphicsPath path = new GraphicsPath();
+            path = GetRoundRectangle(rectangle, r);
+            g.FillPath(b, path);
+            g.DrawPath(p, path);
+            p.Dispose();
+        }
+
+        private void FullContentViewForm_Load(object sender, EventArgs e)
+        {
+            this.TransparencyKey = Color.FromArgb(1, 1, 1);
+            this.BackColor = Color.FromArgb(1, 1, 1);
+
+        }
+
+        private void fullContentMessagePanel_Paint(object sender, PaintEventArgs e)
+        {
+            var x = Location.X;
+        }
+
+        private string content;
+
+        private void label1_Resize(object sender, EventArgs e)
+        {
+            var x = label1.Size;
+        }
+
+        private Font titleFont;
+        private Brush titleBrush;
+        private Rectangle rec;
+        private StringFormat SFormat;
+        static private int cnt = 0, headCnt=0;
+
+        private void fullContentMessagePanel_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+    }
+}
